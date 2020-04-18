e2a7037d0c16e2576ce25ee846977d71d533c671
Bruno <bruno.predot@gmail.com>
2020-04-18 18:14:04 +0200

    refactor spinner

--- src/container/LoadButton/LoadButton.jsx
+++ src/container/LoadButton/LoadButton.jsx
@@ -3,33 +3,20 @@
  * container/Poster
  */
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
-import { library } from '@fortawesome/fontawesome-svg-core';
 import '../../css/LoadButton.css';
-
-library.add(faSpinner);
+import { spinner } from '../Spinner/Spinner';
 
 class LoadButton extends React.Component {
   render() {
     return (
         <React.Fragment>
-            {console.log("loadin :",this.props.loading)}
-          {this.props.loading ?
-
-              <FontAwesomeIcon
-                  icon="spinner"
-                  pulse
-                  size="7x"
-                  className="fa-faSpinner"
-              />
-             :
-
-              <div className="loadButton">
-                <h3 className="loadButton--text">VOIR PLUS</h3>
-              </div>
-
-          }
+            {this.props.loading ? (
+                spinner
+            ) : (
+                <div className="loadButton">
+                    <h3 className="loadButton--text">VOIR PLUS</h3>
+                </div>
+            )}
         </React.Fragment>
     );
   }
---
+++ src/container/Spinner/Spinner.js
@@ -0,0 +1,10 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+library.add(faSpinner);
+
+export const spinner = (
+    <FontAwesomeIcon icon="spinner" pulse size="7x" className="fa-faSpinner" />
+);
