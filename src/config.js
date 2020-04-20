510f0cea4f5a0b42ec52a7271189eb8137c3f960
Bruno <bruno.predot@gmail.com>
2020-04-20 10:40:27 +0200

    add config.js

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export const API_URL = 'https://api.themoviedb.org/3';
+
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p';
+
+export const API_KEY = '8cde946cfc03d7d83678aaf0f4b3f7a4';
+
+export const BACKDROP_SIZE = 'w1280';
+
+export const POSTER_SIZE = 'w500';
